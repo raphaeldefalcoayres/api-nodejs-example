68afc4273851159da84bb12671736bedb940e2a4
Raphael de Falco Ayres <raphaeldefalcoayres@gmail.com>
2019-12-06 16:11:22 -0300

    configuration tests complete basic user ok

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,7 +1,13 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import truncate from '../util/truncate';
+
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
@@ -11,5 +17,25 @@ describe('User', () => {
         password: '123123',
       });
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to register with duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'User test 1',
+        email: 'user1@test.com',
+        password: '123123',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'User test 1',
+        email: 'user1@test.com',
+        password: '123123',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
